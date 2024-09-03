e8dce70210dd6841e4655a1aeb949caf9612e22c
Heitor Ferreira <heitor.ferreira231203@gmail.com>
2024-09-03 17:08:53 -0300

    Melhorando parametros do consumidor com pre-definicoes já configuradas 03.09.2024

--- str-consumer/src/main/java/com/heitor/str_consumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/heitor/str_consumer/listeners/StrConsumerListener.java
@@ -1,29 +1,30 @@
 package com.heitor.str_consumer.listeners;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.heitor.str_consumer.custom.StrConsumerCustomListener;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
-    
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
 
         log.info("CREATE ::: Receive message {}", message);
 
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message) {
 
         log.info("LOG ::: Receive message {}", message);
 
     }
 
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-2")
     public void history(String message) {
 
         log.info("HISTORY ::: Receive message {}", message);
