5195e51e0868cb12a91d8d4203512a452d39a6fb
Heitor Ferreira <heitor.ferreira231203@gmail.com>
2024-09-03 15:50:15 -0300

    Adicionando modelos de configuracao, de construtor e de coletor 03.09.2024

---
+++ str-producer/src/main/java/com/heitor/str_producer/services/StringProducerService.java
@@ -0,0 +1,38 @@
+package com.heitor.str_producer.services;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+
+        kafkaTemplate.send("str-topic", message).addCallback(
+            
+            success -> {
+
+                if(success != null) {
+
+                    log.info("Send message with success {}", message);
+                    log.info("Partition {}", success.getRecordMetadata().partition());
+                    log.info("Offset {}", success.getRecordMetadata().offset());
+
+                }
+
+            },
+            error -> log.error("Error send message")
+
+
+        );
+
+    }
+    
+}
